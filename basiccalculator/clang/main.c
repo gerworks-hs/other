b215ff904ffcb9b940ed0c1f46b89f1d3065181a
itsgerliz <gerworks.hs@gmail.com>
2024-04-15 18:57:14 +0200

    Finished operator selection with error handling

--- basiccalculator/clang/main.c
+++ basiccalculator/clang/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void printWelcome() { //Welcome function
 	printf(
@@ -8,26 +9,28 @@ void printWelcome() { //Welcome function
 	);
 }
 
-int chooseOperator() {
+int chooseOperator() { //Operator selection function
 	char operator[2];
-	printf("Choose the operation that you want to perform -> ");
-	fgets(operator, sizeof(operator), stdin);
-	operator[strcspn(operator, "\n")] == 0;
-	if (strcmp(operator, "+") == 0) {
-		printf("You selected %s\n", operator);
-		return 1;
-	} else if (strcmp(operator, "-") == 0) {
-		printf("You selected %s\n", operator);
-		return 2;
-	} else if (strcmp(operator, "*") == 0) {
-		printf("You selected %s\n", operator);
-		return 3;
-	} else if (strcmp(operator, "/") == 0) {
-		printf("You selected %s\n", operator);
-		return 4;
-	} else {
-		printf("Not a valid option\n");
-		return 0;
+	int c;
+	while (true) {	
+		printf("Choose the operation that you want to perform -> ");
+		fgets(operator, sizeof(operator), stdin); //Pick up user input
+		if (strcmp(operator, "+") == 0) { //Conditionals to check operator input
+			printf("You selected %s\n", operator);
+			return 1;
+		} else if (strcmp(operator, "-") == 0) {
+			printf("You selected %s\n", operator);
+			return 2;
+		} else if (strcmp(operator, "*") == 0) {
+			printf("You selected %s\n", operator);
+			return 3;
+		} else if (strcmp(operator, "/") == 0) {
+			printf("You selected %s\n", operator);
+			return 4;
+		} else {
+			printf("Not a valid option\n");
+			while((c=getchar()) != '\n') {}
+		}
 	}
 }
 
