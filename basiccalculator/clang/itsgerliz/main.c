3a2689df5fcf08b4e87fe8f5d9e0facc9efb6e65
itsgerliz <gerworks.hs@gmail.com>
2024-04-19 18:51:05 +0200

    Started rewrite of the code

--- basiccalculator/clang/itsgerliz/main.c
+++ basiccalculator/clang/itsgerliz/main.c
@@ -1,66 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
-void printWelcome() { //Welcome function
+void printWelcome() {
 	printf(
 		"Welcome to the Basic Calculator\n"
 		"Currently we support (+ - * /)\n"	
 	);
 }
 
-void flushStdin() {
+void flushBuffer() {
 	int c;
-	while ((c=getchar()) != '\n') {}
-}
-
-char firstNum() {
-	char num1[2];
-	printf("Enter the first number -> ");
-	flushStdin();
-	fgets(num1, sizeof(num1), stdin);
-	printf("The first number is %s\n", num1);
-	return *num1;
-}
-
-char secondNum() {
-	char num2[2];
-	printf("Enter the second number -> ");
-	flushStdin();
-	fgets(num2, sizeof(num2), stdin);
-	printf("The second number is %s\n", num2);
-	return *num2;
-}
-
-
-int operator() { //Operator selection function
-	char operator[2];
-	while (true) { //Start of loop
-		printf("Choose the operation that you want to perform -> ");
-		fgets(operator, sizeof(operator), stdin); //Pick up user input
-		if (strcmp(operator, "+") == 0) { //Conditionals to check operator input
-			printf("You selected %s\n", operator);
-			return 1;
-		} else if (strcmp(operator, "-") == 0) {
-			printf("You selected %s\n", operator);
-			return 2;
-		} else if (strcmp(operator, "*") == 0) {
-			printf("You selected %s\n", operator);
-			return 3;
-		} else if (strcmp(operator, "/") == 0) {
-			printf("You selected %s\n", operator);
-			return 4;
-		} else {
-			printf("Not a valid option\n");
-			flushStdin();
-		}
-	}
+	while ((c=getchar()) != '\n' && c != EOF) {}
 }
 
 int main() {
 	printWelcome();
-	operator();
-	firstNum();
-	secondNum();
-	return 0; //Main return
+	return 0;
 }
